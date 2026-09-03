5dedcae06b242e183bb54c8f43eeff4d7d98cde4
vinylhousegarage <vinylhousegarage@gmail.com>
2026-09-03 17:40:54 +0900

    refactor(frontend/auth): migrate authentication to BFF

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_COGNITO_DOMAIN: string;
-  readonly VITE_COGNITO_REDIRECT_SIGN_IN: string;
-  readonly VITE_COGNITO_REDIRECT_SIGN_OUT: string;
-  readonly VITE_COGNITO_USER_POOL_CLIENT_ID: string;
-  readonly VITE_COGNITO_USER_POOL_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
